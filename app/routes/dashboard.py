d1ce0eed7d9b0586d6eaee0b39834bed3181e580
Abdullah Rana <ranaabdullah228.ar@gmail.com>
2025-11-22 23:46:47 +0500

    init.py added each folder

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,7 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, session, redirect, url_for
 
-dashboard_bp = Blueprint('dashboard', __name__, url_prefix="/dashboard")
+dashboard_bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 
 @dashboard_bp.route("/")
 def dashboard():
-    return "Dashboard Page Working"
+    if "user" not in session:
+        return redirect(url_for("auth.login"))
+
+    return "Dashboard Page (Logged in)"
