5c85e39fc29544d62e27667f23b7168d03451a62
Abdullah Rana <ranaabdullah228.ar@gmail.com>
2025-11-23 16:11:44 +0500

    Protoyping done old (throwaway)

--- manager/user_manager.py
+++
@@ -1,9 +0,0 @@
-class UserManager:
-
-    @staticmethod
-    def authenticate(username, password):
-        # Temporary static login for testing
-        if username == "admin" and password == "admin123":
-            return {"username": "admin", "role": "admin"}
-
-        return None
